f8df1832aefe6d0a6a09e5c7a2c621a89522d488
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-03-11 00:03:40 -0300

    adicionei comentarios

--- P_Cinema.c
+++ P_Cinema.c
@@ -44,52 +44,54 @@ struct valorAssento{//a quantidade de assentos
     bool vaga;//vaga da poltrona
 };
 
-int main(){
-    system("clear");
+int main(){//função principal
+
+    system("clear");//apaga terminal
 
     printf("\n Qual nome do Filme? com no max 10 caracters\n");
-    char nomeFilm[101];
-    fgets(nomeFilm, sizeof(nomeFilm), stdin);
-    nomeFilm[strcspn(nomeFilm, "\n")] = '\0';
+    char nomeFilm[101];//nome do filme
+    fgets(nomeFilm, sizeof(nomeFilm), stdin);//le no terminal o nome
+    nomeFilm[strcspn(nomeFilm, "\n")] = '\0';//coloca o fim na string
 
-    sleep(1);
-    system("clear");
+    sleep(1);//espera 1 segundo
+    system("clear");//apaga terminal
 
     short linhas = 0;
     short colunas = 0;
     do{
         printf("\nQuantas colunas? com no maximo 26 colunas\n");
-        scanf("%hd", &colunas);
-        if(colunas > 26 || colunas < 1){
+        scanf("%hd", &colunas);//lê a quantidade de colunas escolhidas
+        if(colunas > 26 || colunas < 1){//caso dê erro
             printf("Você digitou o numero errado, tente novamente.");
-            sleep(2.5);
+            sleep(1.5);
         }else{
-            break;
+            break;//sai do while
         };
-    }while(true);
+    }while(true);//sempre como verdadeiro
     
     do{
-        sleep(1);
+        sleep(1);//espera 1 segudo
         printf("\nQuantas linhas?\n");
-        scanf("%hd", &linhas);
-        if(linhas < 1){
+        scanf("%hd", &linhas);//lê a quantidade de linhas escolhidas
+        if(linhas < 1){//se a quantidade for igual a zero ou negativa
             printf("Você digitou o numero errado, tente novamente.");
-            sleep(2.5);
+            sleep(1.5);
         }else{
-            break;
+            break;//sai do while
         };
     }while(true);
 
-    sleep(5);
+    sleep(2.5);
 
-    struct valorAssento PosiAssento[colunas][linhas];
+    struct valorAssento PosiAssento[colunas][linhas];//cria um array com 2 dimensôes dentro da estrutura 'valorAssento'
     
-    for(int i = 0; i < colunas; i++){
-        for (int j = 0; j < linhas; j++){
+    for(int i = 0; i < colunas; i++){//escrevi o nome e cada coluna
+        for (int j = 0; j < linhas; j++){//escrevi o nome de cada linha
             PosiAssento[i][j].nome = j + 1;
         };
     };
-    for(int i = 0; i < colunas; i++){
+
+    for(int i = 0; i < colunas; i++){//coloca todas poltronas como falsa de começo
         for (int j = 0; j < linhas; j++){
             PosiAssento[i][j].vaga = false;
         };
@@ -100,17 +102,17 @@ int main(){
         printf("Você quer alugar quantos bancos?");
         scanf("%hd", &qtdBancoPAlugar);
 
-        if(qtdBancoPAlugar < 1){
+        if(qtdBancoPAlugar < 1){//finaliza codigo caso não queira alugar nenhum banco
             return 0;
-        }else if(qtdBancoPAlugar < colunas * linhas && qtdBancoPAlugar >= 1){
+        }else if(qtdBancoPAlugar < colunas * linhas && qtdBancoPAlugar >= 1){//caso a quantidadeEscolhida < quantidade de poltronas disponiveis sai do while
             break;
-        }else if(qtdBancoPAlugar > colunas * linhas){
+        }else if(qtdBancoPAlugar > colunas * linhas){//caso exsede o numero de poltronas
             system("clear");
             printf("Não temos tantos bancos, sorry");
             sleep(2);
-        }else if(qtdBancoPAlugar == colunas * linhas){
+        }else if(qtdBancoPAlugar == colunas * linhas){//caso alugue todas poltronas
             printf("\n\nVocê alugou todos bancos.\n\n");
-            for(int i = 0; i < colunas; i++){
+            for(int i = 0; i < colunas; i++){//lista todas poltronas como verdadeiro/alugadas
                 for (int j = 0; j < linhas; j++){
                     PosiAssento[i][j].vaga = true;
                 };
@@ -119,7 +121,7 @@ int main(){
         };
     }while(true);
 
-    for(int l = 1; l <= qtdBancoPAlugar; l++){
+    for(int l = 1; l <= qtdBancoPAlugar; l++){//escolhe as poltronas de acordo com as colunas e linhas
         system("clear");
         short bancoAC = NULL;
         short bancoAL = NULL;
@@ -144,11 +146,10 @@ int main(){
 
         if(PosiAssento[bancoAC][bancoAL].vaga){
             printf("Este ja foi alugado. Tente outro");
-            l = l - 1;
+            l = l - 1;//para repetir o for mais uma vez
         }else{
             PosiAssento[bancoAC][bancoAL].vaga = true;
         };
     };
 
-    //printf("\n\n\n%hd\n\n\n", PosiAssento[colunas - 1][linhas - 1].nome);
 };
